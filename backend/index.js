693ef17a1a8382057f2e491c3eb1028555a45af5
Ruturaj <ruturajnikam1010@gmail.com>
2026-02-24 19:13:01 +0530

    connected the databse at backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+const connectDB = require('./db/db');
 
 const app = express();
 const cors = require("cors");
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const PORT = process.env.PORT;
+
+connectDB();
 
 app.get("/", (req, res)=>{
     res.send("Heyy from backend")
